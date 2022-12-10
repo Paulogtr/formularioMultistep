93ece48d41393ca21a10afe8c1a30ae429a46a36
Paulo Henrique <104275995+Paulogtr@users.noreply.github.com>
2022-12-09 22:41:44 -0300

    criando hook para o projeto

---
+++ formulario/src/App.jsx
@@ -0,0 +1,54 @@
+//components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+import {FiSend} from 'react-icons/fi'
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/Thanks'
+
+// Hooks
+import { useForm } from './hooks/useForm'
+
+import './App.css'
+
+function App() {
+
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
+
+  const { currentStep, currentComponent, changeStep, isLastStep } = useForm(formComponents)
+
+  return (
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulario abaixo para avaliar o produto
+        </p>
+        <div className="form-container">
+          <p>Etapas</p>
+          <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+            <div className="inputs-container">{currentComponent}</div>
+            <div className="actions">
+              <button type='button' onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+              {!isLastStep ? (
+                <button type='submit'>
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              ) : (
+                <button type='button'>
+                  <span>Enviar</span>
+                  <FiSend />
+                </button>)}
+
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
